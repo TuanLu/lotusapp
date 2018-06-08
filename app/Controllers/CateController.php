89fbd19a45395bc2a5523dd747026fc4494eb370
Tim <tuan.mrbean@gmail.com>
2018-06-08 10:20:48 +0700

    update cate manage

--- app/Controllers/CateController.php
+++ app/Controllers/CateController.php
@@ -19,7 +19,6 @@ class CateController extends BaseController
 		// Columns to select.
 		$columns = [
 				'id',
-				'ma_cat',
 				'name',
 				'description'
 		];
@@ -44,17 +43,10 @@ class CateController extends BaseController
 		// Get params and validate them here.
 		//$params = $request->getParams();
 		$id = $request->getParam('id');
-		//die($id);
-		$maCat = $request->getParam('ma_cat');
 		$name = $request->getParam('name');
 		$address = $request->getParam('description');
 		if(!$id) {
 			//Insert new data to db
-			if(!$maCat) {
-				$rsData['message'] = 'Mã danh mục không được để trống!';
-				echo json_encode($rsData);
-				die;
-			}
 			if(!$name) {
 				$rsData['message'] = 'Tên danh mục không được để trống!';
 				echo json_encode($rsData);
@@ -62,40 +54,31 @@ class CateController extends BaseController
 			}
 			$date = new \DateTime();
 			$itemData = [
-				'ma_cat' => $maCat,
 				'name' => $name,
 				'description' => $address,
 				'create_on' => $date->format('Y-m-d H:i:s'),
 			];
-			
 			$result = $this->db->insert($this->tableName, $itemData);
-			$selectColumns = ['id', 'ma_cat'];
-			$where = ['ma_cat' => $itemData['ma_cat']];
 			if($result->rowCount()) {
 				$rsData['status'] = 'success';
 				$rsData['message'] = 'Đã thêm danh mục mới thành công!';
-				$data = $this->db->select($this->tableName, $selectColumns, $where);
-				$rsData['data'] = $data;
+				$rsData['data'] = [
+					'id' => $this->db->id()
+				];
 			} else {
-				$data = $this->db->select($this->tableName, $selectColumns, $where);
-				if(!empty($data)) {
-					$rsData['message'] = "Mã danh mục [". $itemData['ma_cat'] ."] đã tồn tại: ";
-				} else {
-					$rsData['message'] = 'Dữ liệu chưa được cập nhật vào cơ sở dữ liệu!';
-				}
+				$rsData['message'] = 'Dữ liệu chưa được cập nhật vào cơ sở dữ liệu!';
 			}
 		} else {
 			//update data base on $id
 			$date = new \DateTime();
 			$itemData = [
-				'ma_cat' => $maCat,
 				'name' => $name,
 				'description' => $address,
 				'update_on' => $date->format('Y-m-d H:i:s'),
 			];
 			$result = $this->db->update($this->tableName, $itemData, ['id' => $id]);
 			if($result->rowCount()) {
-				$this->superLog('Update NPP', 0, $itemData);
+				$this->superLog('Update Cate', 0, $itemData);
 				$rsData['status'] = self::SUCCESS_STATUS;
 				$rsData['message'] = 'Dữ liệu đã được cập nhật vào hệ thống!';
 			} else {
