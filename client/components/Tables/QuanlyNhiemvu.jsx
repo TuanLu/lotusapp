fe2ab1ff695a4e08323d24bf4969b056942e0c70
Tim <tuan.mrbean@gmail.com>
2018-07-30 12:12:25 +0700

    start my gantt

---
+++ client/components/Tables/QuanlyNhiemvu.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { 
+  Table, Input, Select, 
+  Popconfirm, Form, Row, 
+  Col, Button, message, Modal
+} from 'antd';
+import {getTokenHeader, statusOptions} from 'ISD_API'
+import {updateStateData} from 'actions'
+import UserForm from './User/MyInfoForm'
+
+const FormItem = Form.Item;
+
+class MyMission extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    
+  }
+  render() {
+    let {mainState} = this.props;
+
+    return (
+      <React.Fragment>
+        <div className="table-operations">
+          <Row>
+            <Col span={12}>
+              <h2 className="head-title">Thông tin cá nhân</h2>
+            </Col>
+            <Col span={12}>
+              <div className="action-btns">
+                {/* <Button 
+                  //onClick={() => this.addNewRow()}
+                  type="primary" icon="mail">Góp ý, phản hồi</Button> */}
+              </div>
+            </Col>
+          </Row>
+        </div>
+        <UserForm mainState={this.props.mainState} dispatch={this.props.dispatch}/>
+      </React.Fragment>
+    );
+  }
+}
+
+export default MyMission
